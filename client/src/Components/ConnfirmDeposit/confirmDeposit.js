0c26fe481bd055a3fdac05afc9e4cfeb16259fa0
frank99 <cashcold99@gmail.com>
2025-04-02 14:09:02 +0000

    update

--- client/src/Components/ConnfirmDeposit/confirmDeposit.js
+++ client/src/Components/ConnfirmDeposit/confirmDeposit.js
@@ -144,7 +144,7 @@ class ConfirmDeposit extends Component {
 
 
     render() { 
-        const Amount_to_send = this.state.depositAmount * 0.000025
+        const Amount_to_send = this.state.depositAmount * 0.000012
         const { paymentMade, isSubmitting } = this.state;
         return(
             <div className='confirm'>
